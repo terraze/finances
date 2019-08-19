87aff409a8f887494d67c21bdeb765079719f476
José Ricardo Júnior <jjunior@uplandsoftware.com>
2019-08-18 21:19:31 -0300

    Dados de Saldo vindo do Firebase (accounts)

--- src/components/saldo.js
+++ src/components/saldo.js
@@ -2,56 +2,88 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExchangeAlt, faHandHoldingUsd } from "@fortawesome/free-solid-svg-icons";
 import { Row, Col , Card, CardBody, Button } from 'reactstrap';
-import moment from 'moment'
+import FirebaseService from '../services/FirebaseService.js';
+import Finance from "../models/finance";
 
 class Saldo extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            accounts: []
+        }
+
+        this.loadValues = this.loadValues.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadValues();
+    }
+
+    loadValues() {
+        FirebaseService.getAccounts(
+            (dataReceived) => {
+            this.setState(
+                {accounts: dataReceived}
+            )
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(this.state);
+    }
+
     render () {
+        let accounts = this.state.accounts;
         return (
             <div className={"row terra-body"}>
             <Col lg="12">
-                        <h1>Contas</h1>
-                        <br/>
+                <h1>Contas</h1>
+                <br/>
             </Col>
 
-            <Col lg="6">
-                <Card color="link" >
-                    <CardBody>
-                        <Row>
-                            <Col lg="9">
-                                <h2>Conta-Corrente</h2>
-                                <p>NuBank Zé</p>
-                            </Col>
-                            <Col lg="3"className={"terra-right"}>
-                                <Button className={"terra-button terra-icone terra-icone-black"}>
-                                    <FontAwesomeIcon  icon={faHandHoldingUsd} />
-                                </Button>
-                                <Button className={"terra-button terra-icone terra-icone-black"}>
-                                    <FontAwesomeIcon  icon={faExchangeAlt} />
-                                </Button>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <table className={'table terra-table'}>
-                                    <tbody>
-                                        <tr className={"terra-saldo"}>
-                                            <td>
-                                            Saldo
-                                            </td>
-                                            <td>
-                                            R$ 2.000,00
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </Col>
-                            <br/>
-                            <br/>
-                            <br/>
-                        </Row>   
-                    </CardBody>
-                </Card>
-            </Col>
+            { accounts.map((item, i) => (
+                <Col lg="6" key={i}>
+                    <Card color="link" >
+                        <CardBody>
+                            <Row>
+                                <Col lg="9">
+                                    <h2>{accounts[i].title}</h2>
+                                    <img src={require('..//assets/images/bank_icons/' + accounts[i].bank + '.png')} width={80} height={80}></img>
+                                    <p>{accounts[i].name}</p>
+                                </Col>
+                                <Col lg="3"className={"terra-right"}>
+                                    <Button className={"terra-button terra-icone terra-icone-black"}>
+                                        <FontAwesomeIcon  icon={faHandHoldingUsd} />
+                                    </Button>
+                                    <Button className={"terra-button terra-icone terra-icone-black"}>
+                                        <FontAwesomeIcon  icon={faExchangeAlt} />
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <table className={'table terra-table'}>
+                                        <tbody>
+                                            <tr className={"terra-saldo"}>
+                                                <td>
+                                                Saldo
+                                                </td>
+                                                <td>
+                                                    {Finance.format(accounts[i].total)}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </Col>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </Row>
+                        </CardBody>
+                    </Card>
+                </Col>
+            ))}
             </div>
         )
     }
